6331c048c52f044347861d98f1704cedd3b530e3
BigBadMuttuH <bigbadantonio@gmail.com>
2022-12-15 15:58:58 +0300

    Update

--- src/ModelElements/Polygon.java
+++ src/ModelElements/Polygon.java
@@ -1,6 +1,5 @@
 package ModelElements;
 
-
 import Stuff.Point3D;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,19 +8,19 @@ import java.util.List;
  * полигон
  */
 public class Polygon {
-    /**
-     * полигон по трем точкам
-     * @param a
-     * @param b
-     * @param c
-     * @return полигон
-     */
-    public List<Point3D> Points(Point3D a, Point3D b, Point3D c){
-        List<Point3D> point3DList = new ArrayList<>();
-        point3DList.add(a);
-        point3DList.add(b);
-        point3DList.add(c);
+  /**
+   * полигон по трем точкам
+   * @param pointA
+   * @param pointB
+   * @param pointC
+   * @return полигон
+   */
+  public List<Point3D> Points(Point3D pointA, Point3D pointB, Point3D pointC) {
+    List<Point3D> point3DList = new ArrayList<>();
+    point3DList.add(pointA);
+    point3DList.add(pointB);
+    point3DList.add(pointC);
 
-        return point3DList;
-    }
+    return point3DList;
+  }
 };
