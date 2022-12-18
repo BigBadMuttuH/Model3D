b7113cfbbf1a194d86bd82482ac9be3f5e9ef47e
BigBadMuttuH <bigbadantonio@gmail.com>
2022-12-18 17:53:54 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,6 @@ import Stuff.Point3D;
 
 public class Main {
     public static void main(String[] args) {
-        double x = 0.0;
-        double y = 0.0;
-        double z = 0.0;
-        Point3D a = new Point3D(x, y, z);
-        System.out.println(a);
+
     }
 }
